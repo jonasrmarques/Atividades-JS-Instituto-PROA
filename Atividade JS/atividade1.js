b7c74ab1d876188dc3cd18feb439b3cd51470496
jonasrmarques <jonas.marques.ip@gmail.com>
2024-08-16 16:17:33 -0300

    Atividade JS
    Resolução do exercício da atividade 01 de javascript

---
+++ Atividade JS/atividade1.js
@@ -0,0 +1,45 @@
+// Calcule a média de 3 números
+let num1 = 7
+let num2 = 8.5
+let num3 = 4.6
+let resultado = (num1 + num2 + num3) / 3
+console.log(`O resultado é: ${resultado}`)
+// Calcule  a área do retângulo || base e altura
+let base = 8
+let altura = 10
+let area = base * altura
+console.log(area)
+
+// Dado um número, incremente e decremente o seu valor
+let valorA = 15
+let valorB = 20
+let incrementandoValor = ++valorA
+let decrementandoValor = --valorB
+console.log(incrementandoValor)
+console.log(decrementandoValor)
+
+// Dado dois números, calcule o resto da divisão do primeiro pelo segundo
+let divisao1 = 10
+let divisao2 = 2
+let resto = divisao1 % divisao2
+console.log(resto)
+
+// Dado uma temperatura em Fahrenheit, converta-a para celsius.
+let fahrenheit = 90
+let calculoTotal = (fahrenheit - 32) * (5/9)
+console.log("O calculo total é : " + calculoTotal)
+
+// Dado um número, calcule o dobro e a metade desse número
+let calculNum1 = 25
+let dobro = 25 * 2
+let metade = 25 / 2
+console.log(dobro)
+console.log(metade)
+
+
+// Dado dois números, calcule o resultadao da exponenciação do primeiro número elevado ao segundo
+
+let expNum1 = 5
+let expNum2 = 3
+let exponenciacao = expNum1 ** expNum2
+console.log(exponenciacao)
